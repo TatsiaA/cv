12b8474d1c944d6cd15e967ac88dccecee834a5b
TatsiaA <taniokk@mail.ru>
2022-04-10 23:46:27 +0300

    feat: add header

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello');
+const menu = document.querySelector('.header__menu');
+menu.addEventListener('click',(e) => {
+    e.target.closest('.header__menu').classList.toggle('active');
+});
+
+document.body.addEventListener('click', (e) => {
+    if (!e.target.closest('.header__menu')) {
+        menu.classList.remove('active');
+    }
+})
